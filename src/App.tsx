69a1d29fd67bf4cfedb475422d2c974be9cfe40f
Diogo Carvalho <diogofcarvalho95@gmail.com>
2023-11-11 22:37:32 +0000

    remove rocketseat

--- src/App.tsx
+++ src/App.tsx
@@ -38,12 +38,6 @@ export function App() {
         <h1 className="text-xl font-bold">upload.ai</h1>
 
         <div className="flex items-center gap-3">
-          <span className="text-sm text-muted-foreground">
-            Desenvolvido com 💜 no NLW da Rocketseat
-          </span>
-
-          <Separator orientation="vertical" className="h-6" />
-
           <Button variant="outline">
             <Github className="w-4 h-4 mr-2" />
             GitHub
